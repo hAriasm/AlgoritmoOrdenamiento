d04c95076642daeaac1bf925f8052fd10eec5dcc
Edison Pérez <edisonaqp@gmail.com>
2022-08-14 01:01:09 -0500

    HeapSort - 1ra version

---
+++ cpp/heapSort.cpp
@@ -0,0 +1,102 @@
+// C++ program for implementation of Heap Sort
+
+#include <iostream>
+#include <chrono>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// To heapify a subtree rooted with node i
+// which is an index in arr[].
+// n is size of heap
+void heapify(int arr[], int N, int i)
+{
+
+	// Initialize largest as root
+	int largest = i;
+
+	// left = 2*i + 1
+	int l = 2 * i + 1;
+
+	// right = 2*i + 2
+	int r = 2 * i + 2;
+
+	// If left child is larger than root
+	if (l < N && arr[l] > arr[largest])
+		largest = l;
+
+	// If right child is larger than largest
+	// so far
+	if (r < N && arr[r] > arr[largest])
+		largest = r;
+
+	// If largest is not root
+	if (largest != i) {
+		swap(arr[i], arr[largest]);
+
+		// Recursively heapify the affected
+		// sub-tree
+		heapify(arr, N, largest);
+	}
+}
+
+// Main function to do heap sort
+void heapSort(int arr[], int N)
+{
+
+	// Build heap (rearrange array)
+	for (int i = N / 2 - 1; i >= 0; i--)
+		heapify(arr, N, i);
+
+	// One by one extract an element
+	// from heap
+	for (int i = N - 1; i > 0; i--) {
+
+		// Move current root to end
+		swap(arr[0], arr[i]);
+
+		// call max heapify on the reduced heap
+		heapify(arr, i, 0);
+	}
+}
+
+// A utility function to print array of size n
+void printArray(int arr[], int N)
+{
+	for (int i = 0; i < N; ++i)
+		cout << arr[i] << " ";
+	cout << "\n";
+}
+
+// Driver's code
+int main()
+{
+	srand(time(NULL));
+	int N = 10000;
+	int arr[N] = {};
+	
+	for(int i = 0; i < N; i++) {
+		arr[i] = rand()%N;
+	}
+
+	// printArray(arr, N);
+
+	auto begin = chrono::high_resolution_clock::now();
+
+	// Function call
+	heapSort(arr, N);
+	
+	auto end = chrono::high_resolution_clock::now();
+	double total_time = chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count();
+
+	total_time *= 1e-6;
+
+
+	// cout << endl; 
+	// cout << "Sorted array is \n";
+	// printArray(arr, N);
+
+	cout << "Tiempo total: " << fixed << total_time << setprecision(9);
+	
+	return 0;
+}
