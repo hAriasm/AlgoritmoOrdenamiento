e7e28867e60a9692112f7fad278a876046b97ec0
hAriasm <hariasm@unsa.edu.pe>
2022-08-19 00:59:15 -0500

    phyton for

--- python/treeSort.py
+++ python/treeSort.py
@@ -5,6 +5,7 @@
 # Class containing left and
 # right child of current
 # node and key value 
+from tokenize import String
 import numpy as np
 from timeit import default_timer
 import time
@@ -60,158 +61,31 @@ def treeins(arr):
   for i in range(len(arr)):
     insert(arr[i])
  
- 
-# Driver Code
-
-#a2 = np.loadtxt('../data/data_5000.txt', dtype=int)
-#print(a2)
-#inicio = default_timer()
-#treeins(a2)
-#inorderRec(root)
-#fin = default_timer()
-#print( fin-inicio)
-#root= None 
- 
-
-array_100 = np.loadtxt('..\data\data_100.txt', dtype=int)
-inicio = time.time() 
-inorderRec(root)
-fin = time.time()
-t_100= fin-inicio
-print(t_100)
-root= None 
-array_1000 = np.loadtxt('..\data\data_1000.txt', dtype=int)
-inicio = time.time() 
-inorderRec(root)
-fin = time.time()
-t_1000= fin-inicio
-print(t_1000)
-root= None 
-array_2000 = np.loadtxt('..\data\data_2000.txt', dtype=int)
-inicio = time.time() 
-inorderRec(root)
-fin = time.time()
-t_2000= fin-inicio
-print(t_2000)
-root= None 
-array_3000 = np.loadtxt('..\data\data_3000.txt', dtype=int)
-inicio = time.time() 
-inorderRec(root) 
-fin = time.time()
-t_3000= fin-inicio
-print(t_3000)
-
-root= None 
-array_4000 = np.loadtxt('..\data\data_4000.txt', dtype=int)
-inicio = time.time() 
-inorderRec(root)
-fin = time.time() 
-t_4000= fin-inicio
-print(t_4000)
-
-root= None 
-array_5000 = np.loadtxt('..\data\data_5000.txt', dtype=int)
-inicio = time.time() 
-inorderRec(root)
-fin = time.time()
-t_5000= fin-inicio
-print(t_5000)
-root= None 
-array_6000 = np.loadtxt('..\data\data_6000.txt', dtype=int)
-inicio = time.time() 
-inorderRec(root)
-fin = time.time()
-t_6000= fin-inicio
-print(t_6000)
-root= None 
-array_7000 = np.loadtxt('..\data\data_7000.txt', dtype=int)
-inicio = time.time() 
-inorderRec(root)
-fin = time.time()
-t_7000= fin-inicio
-print(t_7000)
-root= None  
-array_8000 = np.loadtxt('..\data\data_8000.txt', dtype=int)
-inicio = time.time() 
-inorderRec(root)
-fin = time.time()
-t_8000= fin-inicio
-print(t_8000)
-root= None  
-array_9000 = np.loadtxt('..\data\data_9000.txt', dtype=int)
-inicio = time.time() 
-inorderRec(root)
-fin = time.time()
-t_9000= fin-inicio
-print(t_9000)
-root= None 
-array_10000 = np.loadtxt('..\data\data_10000.txt', dtype=int)
-inicio = time.time() 
-inorderRec(root)
-fin = time.time()
-t_10000= fin-inicio
-print(t_10000)
-root= None 
-array_20000 = np.loadtxt('..\data\data_20000.txt', dtype=int)
-inicio = time.time() 
-inorderRec(root)
-fin = time.time()
-t_20000= fin-inicio
-print(t_20000)
-root= None 
-array_30000 = np.loadtxt('..\data\data_30000.txt', dtype=int)
-inicio = time.time() 
-inorderRec(root)
-fin = time.time()
-t_30000= fin-inicio
-print(t_30000)
-root= None 
-array_40000 = np.loadtxt('..\data\data_40000.txt', dtype=int)
-inicio = time.time() 
-inorderRec(root)
-fin = time.time()
-t_40000= fin-inicio
-print(t_40000)
-root= None 
-array_50000 = np.loadtxt('..\data\data_50000.txt', dtype=int)
-inicio = time.time() 
-inorderRec(root)
-fin = time.time()
-t_50000= fin-inicio
-print(t_50000)
-root= None 
-array_100000 = np.loadtxt('..\data\data_100000.txt', dtype=int)
-inicio = time.time() 
-inorderRec(root)
-fin = time.time()
-t_100000= fin-inicio
-print(t_100000)
-root= None 
-array_200000 = np.loadtxt('..\data\data_200000.txt', dtype=int)
-inicio = time.time() 
-inorderRec(root)
-fin = time.time()
-t_200000= fin-inicio
-print(t_200000)
-root= None 
-array_300000 = np.loadtxt('..\data\data_300000.txt', dtype=int)
-inicio = time.time() 
-inorderRec(root)
-fin = time.time()
-t_300000= fin-inicio
-print(t_300000)
-root= None 
-array_400000 = np.loadtxt('..\data\data_400000.txt', dtype=int)
-inicio = time.time() 
-inorderRec(root)
-fin = time.time()
-t_400000= fin-inicio
-print(t_400000)
-root= None 
-array_500000 = np.loadtxt('..\data\data_500000.txt', dtype=int)
-inicio = time.time() 
-inorderRec(root)
-fin = time.time()
-t_500000= fin-inicio
-print(t_500000)
-root= None 
+datoss = ["../data/data_100.txt",
+"../data/data_1000.txt",		
+"../data/data_2000.txt",		
+"../data/data_3000.txt",		
+"../data/data_4000.txt",		
+"../data/data_5000.txt",		
+"../data/data_6000.txt",	
+"../data/data_7000.txt",		
+"../data/data_8000.txt",		
+"../data/data_9000.txt",		
+"../data/data_10000.txt",
+"../data/data_20000.txt",		
+"../data/data_30000.txt",		
+"../data/data_40000.txt",		
+"../data/data_50000.txt",		
+"../data/data_100000.txt",		
+"../data/data_200000.txt",		
+"../data/data_300000.txt",		
+"../data/data_400000.txt",
+"../data/data_500000.txt"]
+for x in range(0,len(datoss)):
+  a2 = np.loadtxt(datoss[x], dtype=int)
+  inicio = time.time()
+  inorderRec(root)
+  treeins(a2)
+  fin = time.time()
+  print(  fin-inicio)
+  root = None
