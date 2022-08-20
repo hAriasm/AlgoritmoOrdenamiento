77a5263321f3efc5dfae78f155aaaaf2192568c2
HARMOR286 <haranzamendi@gmail.com>
2022-08-19 23:19:44 -0500

    V3 Mergesort

--- python/mergesort.py
+++ python/mergesort.py
@@ -1,3 +1,12 @@
+#------------------------------
+# LIBRERIAS
+#------------------------------
+import numpy as np
+import time
+
+#------------------------------
+# ALGORITMO DE ORDENAMIENTO
+#------------------------------
 def merge_sort(lista):
     if len (lista) > 1:
         mitad = len(lista) // 2
@@ -29,11 +38,9 @@ def merge_sort(lista):
             j+=1
             k+=1
 
-
-import numpy as np
-import time
-import matplotlib.pyplot as plt
-
+#-----------------------------------------------------------
+# LECTURA DE DATOS DE ARCHIVOS .TXT Y CONVERSION EN ARRAYs
+#-----------------------------------------------------------
 array_100 =np.loadtxt('..\data\data_100.txt',dtype=int)
 array_1000 =np.loadtxt('..\data\data_1000.txt',dtype=int)
 array_2000 =np.loadtxt('..\data\data_2000.txt',dtype=int)
@@ -54,7 +61,10 @@ array_200000 =np.loadtxt('..\data\data_200000.txt',dtype=int)
 array_300000 =np.loadtxt('..\data\data_300000.txt',dtype=int)
 array_400000 =np.loadtxt('..\data\data_400000.txt',dtype=int)
 array_500000 =np.loadtxt('..\data\data_500000.txt',dtype=int)
-array_1000000 =np.loadtxt('..\data\data_1000000.txt',dtype=int)
+
+#-----------------------------------------------------------
+# CONVERSION EN ARRAYs EN LISTAS
+#-----------------------------------------------------------
 lista_100=array_100.tolist()
 lista_1000=array_1000.tolist()
 lista_2000=array_2000.tolist()
@@ -75,132 +85,187 @@ lista_200000=array_200000.tolist()
 lista_300000=array_300000.tolist()
 lista_400000=array_400000.tolist()
 lista_500000=array_500000.tolist()
-lista_1000000=array_1000000.tolist()
 
-# Temporizador
+#------------------------------------------------------------------
+# EJECUCION DE PROGRAMAS DE ORDENAMIENTO PARA LISTAS DE "N" DATOS
+#------------------------------------------------------------------
 
+# Inicio de temporizador 100 datos
 inicio = time.time()
+# Ejecucion de programa de ordenamiento
 merge_sort(lista_100)
 fin=time.time()
 t_100= fin-inicio
+# Impresion de tiempo de ejecucion de programa
 print(t_100)
 
+# Inicio de temporizador 1000 datos
 inicio = time.time()
+# Ejecucion de programa de ordenamiento
 merge_sort(lista_1000)
 fin=time.time()
 t_1000= fin-inicio
+# Impresion de tiempo de ejecucion de programa
 print(t_1000)
 
+# Inicio de temporizador 2000 datos
 inicio = time.time()
+# Ejecucion de programa de ordenamiento
 merge_sort(lista_2000)
 fin=time.time()
 t_2000= fin-inicio
+# Impresion de tiempo de ejecucion de programa
 print(t_2000)
 
+# Inicio de temporizador 3000 datos
 inicio = time.time()
+# Ejecucion de programa de ordenamiento
 merge_sort(lista_3000)
 fin=time.time()
 t_3000= fin-inicio
+# Impresion de tiempo de ejecucion de programa
 print(t_3000)
 
+# Inicio de temporizador 4000 datos
 inicio = time.time()
+# Ejecucion de programa de ordenamiento
 merge_sort(lista_4000)
 fin=time.time()
 t_4000= fin-inicio
+# Impresion de tiempo de ejecucion de programa
 print(t_4000)
 
+# Inicio de temporizador 5000 datos
 inicio = time.time()
+# Ejecucion de programa de ordenamiento
 merge_sort(lista_5000)
 fin=time.time()
 t_5000= fin-inicio
+# Impresion de tiempo de ejecucion de programa
 print(t_5000)
 
+# Inicio de temporizador 6000 datos
 inicio = time.time()
+# Ejecucion de programa de ordenamiento
 merge_sort(lista_6000)
 fin=time.time()
 t_6000= fin-inicio
+# Impresion de tiempo de ejecucion de programa
 print(t_6000)
 
+# Inicio de temporizador 7000 datos
 inicio = time.time()
+# Ejecucion de programa de ordenamiento
 merge_sort(lista_7000)
 fin=time.time()
 t_7000= fin-inicio
+# Impresion de tiempo de ejecucion de programa
 print(t_7000)
 
+# Inicio de temporizador 8000 datos
 inicio = time.time()
+# Ejecucion de programa de ordenamiento
 merge_sort(lista_8000)
 fin=time.time()
 t_8000= fin-inicio
+# Impresion de tiempo de ejecucion de programa
 print(t_8000)
 
+# Inicio de temporizador 9000 datos
 inicio = time.time()
+# Ejecucion de programa de ordenamiento
 merge_sort(lista_9000)
 fin=time.time()
 t_9000= fin-inicio
+# Impresion de tiempo de ejecucion de programa
 print(t_9000)
 
+# Inicio de temporizador 10000 datos
 inicio = time.time()
+# Ejecucion de programa de ordenamiento
 merge_sort(lista_10000)
 fin=time.time()
 t_10000= fin-inicio
+# Impresion de tiempo de ejecucion de programa
 print(t_10000)
 
+# Inicio de temporizador 20000 datos
 inicio = time.time()
+# Ejecucion de programa de ordenamiento
 merge_sort(lista_20000)
 fin=time.time()
 t_20000= fin-inicio
+# Impresion de tiempo de ejecucion de programa
 print(t_20000)
 
+# Inicio de temporizador 30000 datos
 inicio = time.time()
+# Ejecucion de programa de ordenamiento
 merge_sort(lista_30000)
 fin=time.time()
 t_30000= fin-inicio
+# Impresion de tiempo de ejecucion de programa
 print(t_30000)
 
+# Inicio de temporizador 40000 datos
 inicio = time.time()
+# Ejecucion de programa de ordenamiento
 merge_sort(lista_40000)
 fin=time.time()
 t_40000= fin-inicio
+# Impresion de tiempo de ejecucion de programa
 print(t_40000)
 
+# Inicio de temporizador 50000 datos
 inicio = time.time()
+# Ejecucion de programa de ordenamiento
 merge_sort(lista_50000)
 fin=time.time()
 t_50000= fin-inicio
+# Impresion de tiempo de ejecucion de programa
 print(t_50000)
 
+# Inicio de temporizador 100000 datos
 inicio = time.time()
+# Ejecucion de programa de ordenamiento
 merge_sort(lista_100000)
 fin=time.time()
 t_100000= fin-inicio
+# Impresion de tiempo de ejecucion de programa
 print(t_100000)
 
+# Inicio de temporizador 200000 datos
 inicio = time.time()
+# Ejecucion de programa de ordenamiento
 merge_sort(lista_200000)
 fin=time.time()
 t_200000= fin-inicio
+# Impresion de tiempo de ejecucion de programa
 print(t_200000)
 
+# Inicio de temporizador 300000 datos
 inicio = time.time()
+# Ejecucion de programa de ordenamiento
 merge_sort(lista_300000)
 fin=time.time()
 t_300000= fin-inicio
+# Impresion de tiempo de ejecucion de programa
 print(t_300000)
 
+# Inicio de temporizador 400000 datos
 inicio = time.time()
+# Ejecucion de programa de ordenamiento
 merge_sort(lista_400000)
 fin=time.time()
 t_400000= fin-inicio
+# Impresion de tiempo de ejecucion de programa
 print(t_400000)
 
+# Inicio de temporizador 500000 datos
 inicio = time.time()
+# Ejecucion de programa de ordenamiento
 merge_sort(lista_500000)
 fin=time.time()
 t_500000= fin-inicio
+# Impresion de tiempo de ejecucion de programa
 print(t_500000)
-
-# numero_datos = [100,5000,6000,7000,8000,9000,10000,20000,30000,40000,50000,100000,200000,300000,400000,500000,1000000 ]
-# tiempo= [t_100,t_5000,t_6000,t_7000,t_8000,t_9000,t_10000,t_20000,t_30000,t_40000,t_50000,t_100000,t_200000,t_300000,t_400000,t_500000,t_1000000]
-
-# plt.scatter(numero_datos,tiempo)
-# plt.show()
